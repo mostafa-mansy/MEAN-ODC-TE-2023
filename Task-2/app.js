c2e729ed7561d6b4485dbded7f1824e6fb537dc0
mostafa Abdelkarem <mostafa.abdelkarem57@gmail.com>
2022-12-05 23:22:51 +0200

    complete all show & show single

--- Task-2/app.js
+++ Task-2/app.js
@@ -2,25 +2,48 @@ const user = require("./modules/user.module")
 const yargs = require('yargs')
 yargs.command({
     command:"add",
-
+    builder:{
+/*         const fs = require("fs")
+        fs.writeFileSync("data.json", "argv") */
+    },
+/*     builder: {
+        fNum: {
+            describe: 'First Number',
+            demandOption: true,  // Required
+            type: 'number'     
+        },
+        sNum: {  
+            describe: 'Second Number',
+            demandOption: true,
+            type: 'number'
+        }
+    }, */
     handler: function(argv){
+/*         const fs = require("fs")
+        fs.writeFileSync("data.json", argv) */
         user.add(argv)
+       /*  console.log("Result:", (argv.fNum+argv.sNum)) */
+        
     }
 })
-
-
-/* yargs.command({
+ yargs.command({
     command:"showAll",
-    handler: function(argv){
-        user.showAll()
+    handler: function(argv,id){
+        user.showAll(argv,id)
     }
 })
+
+
+
 yargs.command({
     command:"showSingle",
+    builder: { id: {demandOption: true }},
+    
     handler: function(argv){
-        user.showSingle()
-    }
-})
+        user.showSingle(argv,argv.id)
+/*         console.log(argv.id);
+ */    }
+})/*
 yargs.command({
     command:"edit",
     handler: function(argv){
@@ -34,3 +57,7 @@ yargs.command({
     }
 }) */
 yargs.argv
+
+
+const name = "heelo word"
+console.log(name);
--- Task-2/modules/user.module.js
+++ Task-2/modules/user.module.js
@@ -5,6 +5,7 @@ heads = [
     {key:"email", default:null}, 
     {key:"status", default: false}]
 const deal = require("./deal.module")
+const fs = require("fs")
 class User{
     static add(data){
         const user = {}
@@ -17,10 +18,42 @@ class User{
         const all = deal.readFromJson()
         all.push(user)
         deal.writeToJson(all)
-        
     }
-    static showAll(){}
-    static showSingle(){}
+    static showAll(view){
+        view = deal.readFromJson("data.json")
+        console.log(view);
+// run tamm
+        /* console.log(require('../data.json')) */
+// run tamm
+/*         fs.readFile('data.json', (err, data) => {
+            if (err) throw err;
+            let student = JSON.parse(data);
+            console.log(student);
+        }); */
+    }
+    static showSingle(showdata,idnum){
+        showdata = deal.readFromJson("data.json")
+ /*        showdata.forEach(
+            (idnum=showdata.id) ? console.log(showdata.id) : console.log("Not Found")
+        ) */
+/*           for(let sunm in showdata){
+            (showdata.id=idnum) ? console.log(showdata) : console.log("Not Found")
+          }  */ 
+/*           let viewSingle = showdata.find(id => (idnum=showdata.id) ? console.log(showdata['idnum']) : console.log("Not Found")
+          )  
+          console.log(viewSingle) */
+          
+/*           showdata.forEach( id => {
+            
+            (idnum=showdata.id) ? console.log(showdata) : console.log("Not Found")
+            
+        })  */
+          showdata.find(
+            (el)=> (el.id===idnum)? console.log(el) : null
+          )
+ 
+
+    }
     static edit(){}
     static del(){}
 }
